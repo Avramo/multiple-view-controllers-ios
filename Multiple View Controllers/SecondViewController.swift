0ae405cb7d36dd39dc7a8fba5c21d0cd86000039
Avraham Kirsch <etrog95@gmail.com>
2020-09-29 14:30:00 +0300

    first commit

---
+++ Multiple View Controllers/SecondViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SecondViewController.swift
+//  Multiple View Controllers
+//
+//  Created by admin on 26/01/2020.
+//  Copyright © 2020 AM Kirsch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("second view controller loaded")
+    }
+
+
+}
+
